0a3ec38d9a831f72d573f30298f67bcbc6c65959
GITnonameismyname <118059294+GITnonameismyname@users.noreply.github.com>
2026-08-10 00:10:25 +0100

    Create forwardMessage.js
      -Meshtastic messages forwarded to SignalK path
      
      Read mesh messages in SignalK path "communication.meshtastic.rx"
      
      Now SignalK plugins or other Webapps like Node-Red can read messages from the mesh.

---
+++ plugin/forwardMessage.js
@@ -0,0 +1,28 @@
+module.exports = (app, message) => {
+  if (!message || !message.data) {
+    return;
+  }
+
+  app.handleMessage('signalk-meshtastic', {
+    context: 'vessels.self',
+    updates: [
+      {
+        source: {
+          label: 'signalk-meshtastic',
+          src: String(message.from),
+        },
+        timestamp: new Date().toISOString(),
+        values: [
+          {
+            path: 'communication.meshtastic.rx',
+            value: {
+                text: message.data,
+                from: message.from,
+                timestamp: new Date().toISOString(),
+            }
+          },
+        ],
+      },
+    ],
+  });
+};
