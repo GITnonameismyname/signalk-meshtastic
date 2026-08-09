6bf7c4d5688aa9daaec3df2a20015cb30adde0e6
GITnonameismyname <118059294+GITnonameismyname@users.noreply.github.com>
2026-08-10 00:06:48 +0100

    Create sendMessage.js
    -Meshtastic message transmission
      Direct messages can be sent using a Meshtastic Node ID.
      Short Name → Node ID resolution has been added.
      Broadcast messaging is supported.
      
      Send you message to SignalK path "communication.meshtastic.tx"
      
      Now SignalK plugins or other Webapps like Node-Red can send messages or alarms to the mesh.
      
      Node-red example flow.
      
      [{"id":"9cae7ea103672681","type":"signalk-send-delta","z":"9245b74e1806be98","name":"communication.meshtastic.tx","x":760,"y":5980,"wires":[]},{"id":"921bb5e7a797f503","type":"inject","z":"9245b74e1806be98","name":"to all","props":[{"p":"payload"},{"p":"topic","vt":"str"}],"repeat":"","crontab":"","once":false,"onceDelay":0.1,"topic":"","payload":"{\"context\":\"vessels.self\",\"updates\":[{\"values\":[{\"path\":\"communication.meshtastic.tx\",\"value\":{\"text\":\"Test from Node RED\",\"to\":\"broadcast\"}}]}]}","payloadType":"json","x":150,"y":5980,"wires":[["9cae7ea103672681"]]},{"id":"576c5265e31481cf","type":"inject","z":"9245b74e1806be98","name":"to 1191614505  , Meshtastic 9829","props":[{"p":"payload"},{"p":"topic","vt":"str"}],"repeat":"","crontab":"","once":false,"onceDelay":0.1,"topic":"","payload":"{\"context\":\"vessels.self\",\"updates\":[{\"values\":[{\"path\":\"communication.meshtastic.tx\",\"value\":{\"text\":\"Test from Node RED\",\"to\":1191614505}}]}]}","payloadType":"json","x":230,"y":6040,"wires":[["9cae7ea103672681"]]},{"id":"cb23487b35b1c6f2","type":"inject","z":"9245b74e1806be98","name":"to 9829  , Meshtastic 9829","props":[{"p":"payload"},{"p":"topic","vt":"str"}],"repeat":"","crontab":"","once":false,"onceDelay":0.1,"topic":"","payload":"{\"context\":\"vessels.self\",\"updates\":[{\"values\":[{\"path\":\"communication.meshtastic.tx\",\"value\":{\"text\":\"Test from Node RED\",\"to\":\"9829\"}}]}]}","payloadType":"json","x":210,"y":6100,"wires":[["9cae7ea103672681"]]},{"id":"b8d76f8a7bf3f079","type":"global-config","env":[],"modules":{"@signalk/node-red-embedded":"2.21.0"}}]

---
+++ plugin/sendMessage.js
@@ -0,0 +1,27 @@
+module.exports = async (device, value, nodes) => {
+  if (!device || !value || !value.text) {
+    return;
+  }
+
+  let destination = value.to || 'broadcast';
+
+  if (typeof destination === 'string'
+      && destination !== 'broadcast'
+      && destination !== 'self') {
+    const nodeId = Object.keys(nodes).find((nodeNum) => {
+      return nodes[nodeNum].shortName === destination;
+    });
+
+    if (!nodeId) {
+      throw new Error(`Meshtastic node with short name "${destination}" not found`);
+    }
+
+    destination = Number(nodeId);
+  }
+
+  await device.sendText(
+    value.text,
+    destination,
+    true,
+  );
+};
